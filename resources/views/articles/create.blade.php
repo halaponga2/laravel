9fa24e58d48329956ce6b9b1471539935319d6fd
Ivan <halaponga2@gmail.com>
2021-11-18 12:26:21 +0300

    A bit of bootstrap

--- resources/views/articles/create.blade.php
+++ resources/views/articles/create.blade.php
@@ -1,11 +1,20 @@
 @extends('layouts.layout')
 @section('content')
 <h1>Article</h1>
-    <form method="post" action="../articles">
-        @csrf 
-        <input placeholder="Введите имя" id="name" name="name" type="text" style="border: 1px solid black;">
-        <input placeholder="Введите текст" id="short_text" name="short_text" type="text" style="border: 1px solid black;">
-        <input id="data_create" name="data_create"  type="date"  style="border: 1px solid black;">
-        <button type="submit">Отправить</button>
+    <form method="post" action="../articles" class="col-md-4 col-sm-12">
+        @csrf
+        <div class="form-group ">
+            <label for="name">Введите имя</label>
+            <input  id="name" name="name" type="text" class="form-control">
+        </div>
+        <div class="form-group mt-3">
+            <label for="short_text">Введите текст</label>
+            <input  id="short_text" name="short_text" type="text" class="form-control">
+        </div>
+        <div class="form-group mt-3">
+            <label for="data_create">Выберите дату</label>
+            <input id="data_create" name="data_create"  type="date"  class="form-control">
+        </div>
+        <button type="submit" class="btn btn-primary mt-3">Отправить</button>
     </form>
 @endsection
--- resources/views/articles/view.blade.php
+++ resources/views/articles/view.blade.php
@@ -1,25 +1,45 @@
 @extends('layouts.layout')
 @section('content')
-    <h2>{{$article->name}}</h2>
-    <i>{{$article->data_create}}</i>
-    <p>{{$article->short_text}}</p>        
-    <h3>Комментарии</h3>
+    <div class="card mt-5 mb-5">
+        <div class="card-body">
+            <p>{{$article->short_text}}</p>
+        </div>
+        <div class="card-footer d-inline-flex justify-content-between">
+            <p>{{$article->name}}<p>
+            <i>{{$article->data_create}}</i>
+        </div>  
+    </div>      
+    @if (!$article->comments->isEmpty())
+        <h3>Комментарии</h3>    
+    @endif
+    
     @foreach($article->comments as $comment)
-        <b>{{$comment->title}}</b>
-        <p>{{$comment->comment}}</p>
+    <div class="d-inline-flex p-2">
+        <div class="card" style="width: 18rem;">
+            <div class="card-header">
+                <h5 class="card-title">{{$comment->title}}</h5>
+            </div>
+            <div class="card-body">
+                
+                <p class="card-text">{{$comment->comment}}</p>
+            </div>
+        </div>
+    </div>
     @endforeach
 
 
 
-    <form action="../articles/{{$article->id}}/comments" method="post">
+    <form action="../articles/{{$article->id}}/comments" method="post" class="col-md-4 col-sm-12">
         @csrf 
         <h3>Добавить комментарий</h3>
-        <div>
-            <input type="text" name="title" id="comment-title" placeholder="Введите заголовок">
-            <input type="text" name="comment" id="comment-text" placeholder="Введите текст">
+        <div class="form-group">
+            <label for="comment-title">Введите заголовок</label>
+            <input type="text" name="title" id="comment-title" class="form-control">
         </div>
-        <div>
-            <button type="submit">Отправить</button>
+        <div class="form-group mt-3">
+            <label for="comment-text">Введите текст</label>
+            <input type="text" name="comment" id="comment-text" class="form-control">
         </div>
+        <button type="submit" class="btn btn-primary mt-3">Отправить</button>
     </form>
 @endsection
